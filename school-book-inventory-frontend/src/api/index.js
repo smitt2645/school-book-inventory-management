97958c67ec2c7761c7e13a403cc55678abc3571f
Your Name <your.email@example.com>
2026-01-01 13:21:17 +0530

    base url change for render !

--- school-book-inventory-frontend/src/api/index.js
+++ school-book-inventory-frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://your-backend-name.onrender.com/api/v1",
+  baseURL: " https://school-management-be-31xm.onrender.com/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
